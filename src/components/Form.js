c0598354f2449398ae471af673b59218940003f8
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-07-14 19:25:28 +0300

    commit tree 579a5e071373614882af2acc006a436c1bfc6e58

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
+
+  // function handleFirstNameChange(e) {
+  //   setFirstName(e.target.value);
+  // }
+  // function handleLastNameChange(e) {
+  //   setLastName(e.target.value);
+  // }
+
+  // experimenting with checked
+  // const [newsletter, setNewsletter] = useState(false);
+
+  // function handleNewsletterChange(e) {
+  //   setNewsletter(e.target.checked);
+  // }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFirstNameChange} />
+      <input type="text" value={lastName} onChange={handleLastNameChange} />
       <button type="submit">Submit</button>
     </form>
+
+    // experimenting with checked
+    // <form>
+    //   <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+    //   <input
+    //     type="checkbox"
+    //     id="newsletter"
+    //     checked={newsletter}
+    //     onChange={handleNewsletterChange}
+    //   />
+    //   <button type="submit">Submit</button>
+    // </form>
   );
 }
 
